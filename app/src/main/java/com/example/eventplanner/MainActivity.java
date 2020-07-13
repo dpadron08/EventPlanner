fb79d727b1ad82f848a53e25ec9020ba5a270dd1
dpadron08 <padroncastillo@gmail.com>
2020-07-13 15:34:16 -0500

    Make fragments and layouts and enable transition between them

--- app/src/main/java/com/example/eventplanner/MainActivity.java
+++ app/src/main/java/com/example/eventplanner/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.eventplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.eventplanner.fragments.FriendsFragment;
+import com.example.eventplanner.fragments.MapFragment;
+import com.example.eventplanner.fragments.ProfileFragment;
+import com.example.eventplanner.fragments.TimelineFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_timeline:
+                        //fragment = fragment1;
+                        fragment = new TimelineFragment();
+                        Toast.makeText(MainActivity.this, "Timeline!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_friends:
+                        //fragment = fragment2;
+                        fragment = new FriendsFragment();
+                        Toast.makeText(MainActivity.this, "Friends!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                        //fragment = fragment3;
+                        fragment = new ProfileFragment();
+                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_map:
+                        Toast.makeText(MainActivity.this, "Map", Toast.LENGTH_SHORT).show();
+                    default:
+                        //fragment = fragment4;
+                        fragment = new MapFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_timeline);
     }
 }
