3464f1483fc48499e02a625be9a3bc757bb8108a
dpadron08 <padroncastillo@gmail.com>
2020-07-16 17:11:09 -0500

    Add ability to take photo to set profile pic

--- app/src/main/java/com/example/eventplanner/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/eventplanner/fragments/ProfileFragment.java
@@ -1,7 +1,9 @@
 package com.example.eventplanner.fragments;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Build;
@@ -9,10 +11,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -69,7 +73,9 @@ public class ProfileFragment extends Fragment {
     private static final String TAG = "ProfileFragment";
     // PICK_PHOTO_CODE is a constant integer
     public final static int PICK_PHOTO_CODE = 1046;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     File photoFile = null;
+    String photoFileNameWithCamera = "photo2";
 
     // for recycler view list of events
     RecyclerView rvEvents;
@@ -81,6 +87,7 @@ public class ProfileFragment extends Fragment {
     TextView tvUsername;
     TextView tvInterests;
     Button btnAddProfilePicture;
+    Button btnTakePicture;
     Button btnLogout;
 
     // for the progress loading action item
@@ -134,6 +141,7 @@ public class ProfileFragment extends Fragment {
         tvUsername = view.findViewById(R.id.tvUsername);
         tvInterests = view.findViewById(R.id.tvInterests);
         btnAddProfilePicture = view.findViewById(R.id.btnAddProfilePicture);
+        btnTakePicture = view.findViewById(R.id.btnTakePicture);
         btnLogout = view.findViewById(R.id.btnLogout);
         queryUserProfile(); // get user attributes and populate the views with data
 
@@ -151,6 +159,12 @@ public class ProfileFragment extends Fragment {
                 onPickPhoto(view);
             }
         });
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,6 +172,43 @@ public class ProfileFragment extends Fragment {
                 userLogout();
             }
         });
+    }
+
+    private void launchCamera() {
+        photoFile = null;
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileNameWithCamera);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.fileproviderAnother", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    private File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        return file;
     }
 
     private void userLogout() {
@@ -302,7 +353,51 @@ public class ProfileFragment extends Fragment {
             saveProfilePic();
 
         }
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                Bitmap bitmapScaled = scaleToFitHeight(takenImage, 200);
 
+                /* Without resizing
+                // Load the taken image into a preview
+                ivPostImage.setImageBitmap(takenImage);
+                 */
+
+                // Write the smaller bitmap back to disk
+                // Configure byte output stream
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                // Compress the image further
+                bitmapScaled.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+                File resizedFile = getPhotoFileUri(photoFileNameWithCamera + "_resized");
+                try {
+                    resizedFile.createNewFile();
+                    FileOutputStream fos = new FileOutputStream(resizedFile);
+                    // Write the bytes of the bitmap to file
+                    fos.write(bytes.toByteArray());
+                    fos.close();
+                } catch (Exception e) {
+                    Log.e(TAG, "Problem writing to disk");
+                }
+
+                ivProfilePic.setImageBitmap(bitmapScaled);
+                saveProfilePic();
+
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    // Scale and maintain aspect ratio given a desired height
+    // BitmapScaler.scaleToFitHeight(bitmap, 100);
+    public static Bitmap scaleToFitHeight(Bitmap b, int height)
+    {
+        float factor = height / (float) b.getHeight();
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factor), height, true);
     }
 
     private void saveProfilePic() {
