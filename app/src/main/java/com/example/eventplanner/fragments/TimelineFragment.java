5998063847634999fc0da3170155a8cc6c819df8
dpadron08 <padroncastillo@gmail.com>
2020-07-29 15:21:19 -0500

    Remove extraneous adapter refreshing

--- app/src/main/java/com/example/eventplanner/fragments/TimelineFragment.java
+++ app/src/main/java/com/example/eventplanner/fragments/TimelineFragment.java
@@ -218,7 +218,7 @@ public class TimelineFragment extends Fragment {
                     Log.e(TAG, "Failed to query events");
                     return;
                 }
-                
+
                 // for debugging purposes let's print every event description to logcat
                 /*
                 for (Event event : objects) {
@@ -263,7 +263,7 @@ public class TimelineFragment extends Fragment {
                 totalQueried += objects.size();
                 allEvents.addAll(objects);
                 removeEventsAtCapacity();
-                adapter.notifyDataSetChanged();
+                //adapter.notifyDataSetChanged();
             }
         });
 
